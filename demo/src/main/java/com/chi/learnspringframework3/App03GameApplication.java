0a18f65b6fccbec12943008e6f3d6a7a06198c69
vickyh7518 <vvv488322@gmail.com>
2024-06-27 00:33:09 +0800

    [edit] App03 (整合 Configuration)

--- demo/src/main/java/com/chi/learnspringframework3/App03GameApplication.java
+++ demo/src/main/java/com/chi/learnspringframework3/App03GameApplication.java
@@ -2,7 +2,25 @@ package com.chi.learnspringframework3;
 
 import com.chi.learnspringframework.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
+
+@Configuration
+class GamingConfiguration {
+    @Bean
+    public GamingConsole game() {
+        var game = new PacmanGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+
+}
 
 public class App03GameApplication {
     public static void main(String[] args) {
