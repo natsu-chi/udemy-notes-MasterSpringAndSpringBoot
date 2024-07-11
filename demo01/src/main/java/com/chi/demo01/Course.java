ff82ed1b20a6af30ec5d4c8f8d66e603dbd223de
vickyh7518 <vvv488322@gmail.com>
2024-07-11 22:16:38 +0800

    Spring MVC & spring-boot-starter-actuator

---
+++ demo01/src/main/java/com/chi/demo01/Course.java
@@ -0,0 +1,27 @@
+package com.chi.demo01;
+
+public class Course {
+    private String courseName;
+    private String courseCode;
+
+    Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                '}';
+    }
+}
---
+++ demo01/src/main/java/com/chi/demo01/CourseController.java
@@ -0,0 +1,28 @@
+package com.chi.demo01;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+//        return List.of(
+//            new Course("Learn AWS", "10001"),
+//            new Course("Learn Spring Boot", "10002")
+//        );
+
+        return Arrays.asList(
+            new Course("Learn AWS", "10001"),
+            new Course("Learn Spring Boot", "10002")
+        );
+
+
+    }
+}
