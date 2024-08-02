2d62d0c5e173c8b92b99ba0ef22eecc32c9d8f6e
vickyh7518 <vvv488322@gmail.com>
2024-08-02 22:00:05 +0800

    Update: Allow cors

--- demo03/src/main/java/com/chi/demo03/Demo03Application.java
+++ demo03/src/main/java/com/chi/demo03/Demo03Application.java
@@ -2,12 +2,28 @@ package com.chi.demo03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Demo03Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo03Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://127.0.0.1:55522", "http://localhost:3000")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
