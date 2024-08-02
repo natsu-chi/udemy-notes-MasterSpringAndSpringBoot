54c2e50d4e18a4ba6af73462d82d8f063f05f93c
vickyh7518 <vvv488322@gmail.com>
2024-08-02 22:02:17 +0800

    Add: Jwt Authentication

---
+++ demo03/src/main/java/com/chi/demo03/jwt/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.chi.demo03.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ demo03/src/main/java/com/chi/demo03/jwt/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.chi.demo03.jwt;
+
+public record JwtTokenResponse(String token) {}
