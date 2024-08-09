b2f11e38b40af51f4e6f879c6a1b6e227cdba1cf
vickyh7518 <vvv488322@gmail.com>
2024-08-09 10:13:45 +0800

    Update: Demo10 - Refactoring of Accounts application

---
+++ demo10/accounts/src/main/java/com/chi/demo10/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,28 @@
+package com.chi.demo10.accounts.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+
+// 自動填充審計欄位:
+// Entity 使用 @CreatedBy 或 @LastModifiedBy 等審計註釋時，
+// Spring Data JPA 會自動呼叫這個類來獲取審計員資訊並填充相應的欄位
+
+// 這邊對應到 BaseEntity
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * Returns the current auditor of the application.
+     *
+     * @return the current auditor.
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNT_MS");
+    }
+    
+}
--- demo10/accounts/src/main/java/com/chi/demo10/accounts/exceptions/CustomerAlreadyExistException.java
+++ demo10/accounts/src/main/java/com/chi/demo10/accounts/exceptions/CustomerAlreadyExistException.java
@@ -1,4 +1,4 @@
-package com.chi.demo10.exceptions;
+package com.chi.demo10.accounts.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
